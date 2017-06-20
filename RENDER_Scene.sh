20ba513e94bad96856ba3dc0d51aaf5aacb0eb3d
Ling Evan <lingevan@EMBP-2.local>
2017-06-20 02:57:22 +0100

    minor

--- RENDER_Scene.sh
+++ RENDER_Scene.sh
@@ -6,10 +6,10 @@ cp {poses.txt,scene_description.txt} /homes/el216/Workspace/DataSceneNet
 find /homes/el216/Workspace/OutputSceneNet -type f -delete
 cp {randomObjectsLocations.txt,roomsLayout+Objects.png} /homes/el216/Workspace/OutputSceneNet
 
-# Run renderer
-cd /homes/el216/Workspace/roboteye/build
-./DynamicPose_SceneNet /homes/el216/Workspace/OutputSceneNet /homes/el216/Workspace/DataSceneNet/poses.txt
-
+# # Run renderer
+# cd /homes/el216/Workspace/roboteye/build
+# ./DynamicPose_SceneNet /homes/el216/Workspace/OutputSceneNet /homes/el216/Workspace/DataSceneNet/poses.txt
+ 
 # Create Output Folder named after houseID
 
 # Copy roomsLayout+Objects.png and randomObjectsLocations.txt
