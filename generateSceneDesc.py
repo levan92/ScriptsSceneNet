d4165b8747b900f04a5e9b689bb1fbfe60e85238
Ling Evan <lingevan@EMBP-2.local>
2017-06-21 17:12:35 +0100

    added rooms info to scene desc

--- generateSceneDesc.py
+++ generateSceneDesc.py
@@ -2,18 +2,47 @@ import numpy as np
 import pickle
 import sys
 
-houseID = sys.argv[1]
-
 f = open('fromRandomObjects.pckl','rb')
 totalNumObjects, objIDs, objWnids, scales, Ts = pickle.load(f)
 f.close()
 
+f2 = open('fromOcMap.pckl','rb')
+[_, numRooms, cellSide, origin_ocMap, _,
+              roomsBBmin, roomsBBmax, roomsSize] = pickle.load(f2)
+f2.close()
+
+#returns world coordinate of the top-left of a given cell
+def cell2WorldCoord_TopLeft(cell):
+    [i,j] = cell
+    z = origin_ocMap[0] + cellSide * i
+    x = origin_ocMap[1] + cellSide * j
+    return np.array([z,x])
+
+#returns world coordinate of the top-left of a given cell
+def cell2WorldCoord_BotRight(cell):
+    [i,j] = cell
+    z = origin_ocMap[0] + cellSide * (i+1)
+    x = origin_ocMap[1] + cellSide * (j+1)
+    return np.array([z,x])
+
+houseID = sys.argv[1]
 houseObj_filepath = 'suncg/house/' + houseID + '/houseOneFloor.obj'
 
+# Header
 w = open("scene_description.txt","w")
 w.write('layout_file: ./')
 w.write(houseObj_filepath + '\n')
 
+# Room Information for lighting
+print >> w, 'rooms'
+for r in range(numRooms):
+    if not ( np.isnan(roomsBBmin).any() or np.isnan(roomsBBmax).any() ):
+        bbMin_zx = cell2WorldCoord_TopLeft(roomsBBmin[r])
+        bbSize_zx = roomsSize[r] * cellSide
+        print >> w, round(bbMin_zx[0],2), round(bbMin_zx[1],2), \
+                round(bbSize_zx[0],2), round(bbSize_zx[1],2)
+
+# Objects
 for obj in range(totalNumObjects):
     w.write('object\n')
     w.write(objWnids[obj] + '/' + objIDs[obj] + '\n')
