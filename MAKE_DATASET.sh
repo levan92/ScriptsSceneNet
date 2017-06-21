d3fc06a96227d5c6e72cb0430d270aff4e81cc12
Ling Evan <lingevan@EMBP-2.local>
2017-06-21 01:56:26 +0100

    ready for next house

--- MAKE_DATASET.sh
+++ MAKE_DATASET.sh
@@ -2,7 +2,9 @@
 
 # User Parameters
 # houseID=fffeb1ec4c22ee4b96aa8f8acc564721
-houseID=a55f64ca8fdee38a554429d7f7ac8b50
+# houseID=a55f64ca8fdee38a554429d7f7ac8b50
+houseID=b007416e1382868be283d228f2e3e4d6
+
 ocMapCellSide=0.1 # in m, must be small enough 
 roomMessMean=40 # in num objs per 100m^2
 roomMessSD=10
@@ -10,45 +12,45 @@ frameStep=20 # for poses, Frame period = frameStep * 0.1s
 
 echo 'houseID: '$houseID | tee makedataset_logfile.log
 
-# # Generate .obj and .mtl files from .json
-# cd /homes/el216/Workspace/DataSceneNet/Layouts/suncg/house/$houseID
-# /homes/el216/Workspace/SUNCGtoolbox/gaps/bin/x86_64/scn2scn house.json house.obj
+# Generate .obj and .mtl files from .json
+cd /homes/el216/Workspace/DataSceneNet/Layouts/suncg/house/$houseID
+/homes/el216/Workspace/SUNCGtoolbox/gaps/bin/x86_64/scn2scn house.json house.obj
 
 
-# cd /homes/el216/Workspace/ScriptsSceneNet
-# # Convert .obj file to only one floor
-# python -u convertToOneFloorObj.py \
-# 	/homes/el216/Workspace/DataSceneNet/Layouts/suncg/house/$houseID | tee -a makedataset_logfile.log
-# # Create occupancy map from house obj
-# # Outputs: fromOcMap.pckl, roomsLayout.png
-# python -u occupancyMap.py \
-# 	/homes/el216/Workspace/DataSceneNet/Layouts/suncg/house/$houseID \
-# 	$ocMapCellSide | tee -a makedataset_logfile.log
-# # Generate random objects for house
-# # Arguments: Room Messiness Mean, SD in num objs per 100m^2
-# # Outputs: fromRandomObjects.pckl, roomsLayout+Objects.png, randomObjectsLocations.txt
-# python -u randomObjects.py $roomMessMean $roomMessSD | tee -a makedataset_logfile.log
-# # Generate SceneDescription txt from random objects
-# # Outputs: scene_description.txt
-# python -u generateSceneDesc.py $houseID | tee -a makedataset_logfile.log
-# # Generate Poses.txt from room info from occupancymap.py
-# # Outputs: poses.txt
-# python -u generatePoses.py $frameStep | tee -a makedataset_logfile.log
+cd /homes/el216/Workspace/ScriptsSceneNet
+# Convert .obj file to only one floor
+python -u convertToOneFloorObj.py \
+	/homes/el216/Workspace/DataSceneNet/Layouts/suncg/house/$houseID | tee -a makedataset_logfile.log
+# Create occupancy map from house obj
+# Outputs: fromOcMap.pckl, roomsLayout.png
+python -u occupancyMap.py \
+	/homes/el216/Workspace/DataSceneNet/Layouts/suncg/house/$houseID \
+	$ocMapCellSide | tee -a makedataset_logfile.log
+# Generate random objects for house
+# Arguments: Room Messiness Mean, SD in num objs per 100m^2
+# Outputs: fromRandomObjects.pckl, roomsLayout+Objects.png, randomObjectsLocations.txt
+python -u randomObjects.py $roomMessMean $roomMessSD | tee -a makedataset_logfile.log
+# Generate SceneDescription txt from random objects
+# Outputs: scene_description.txt
+python -u generateSceneDesc.py $houseID | tee -a makedataset_logfile.log
+# Generate Poses.txt from room info from occupancymap.py
+# Outputs: poses.txt
+python -u generatePoses.py $frameStep | tee -a makedataset_logfile.log
 
 
-# # Copy generated files to respective directories
-# cp poses.txt /homes/el216/Workspace/DataSceneNet
-# cp scene_description.txt /homes/el216/Workspace/DataSceneNet
-# find /homes/el216/Workspace/OutputSceneNet -type f -delete
-# cp randomObjectsLocations.txt /homes/el216/Workspace/OutputSceneNet
-# cp roomsLayout+Objects.png /homes/el216/Workspace/OutputSceneNet
+# Copy generated files to respective directories
+cp poses.txt /homes/el216/Workspace/DataSceneNet
+cp scene_description.txt /homes/el216/Workspace/DataSceneNet
+find /homes/el216/Workspace/OutputSceneNet -type f -delete
+cp randomObjectsLocations.txt /homes/el216/Workspace/OutputSceneNet
+cp roomsLayout+Objects.png /homes/el216/Workspace/OutputSceneNet
 
 
-# # Run renderer
-# cd /homes/el216/Workspace/roboteye/build
-# ./DynamicPose_SceneNet /homes/el216/Workspace/OutputSceneNet \
-# /homes/el216/Workspace/DataSceneNet/scene_description.txt \ 
-# | tee -a /homes/el216/Workspace/ScriptsSceneNet/makedataset_logfile.log
+# Run renderer
+cd /homes/el216/Workspace/roboteye/build
+./DynamicPose_SceneNet /homes/el216/Workspace/OutputSceneNet \
+/homes/el216/Workspace/DataSceneNet/scene_description.txt \ 
+| tee -a /homes/el216/Workspace/ScriptsSceneNet/makedataset_logfile.log
 
 cd /homes/el216/Workspace/ScriptsSceneNet
 echo 'Frames rendering completed.' | tee -a makedataset_logfile.log
