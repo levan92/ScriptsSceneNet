700b03f4351ef83bfbc87ba9d3e771d6e066e344
Evan Ling <el216@sicklebill.doc.ic.ac.uk>
2017-06-23 16:32:33 +0100

    completed editting bash script 2 to handle simultaneous rendering of houses

--- MAKE_DATASET2.sh
+++ MAKE_DATASET2.sh
@@ -6,14 +6,15 @@
 # houseID=fff3ca3254c364df22f15646ad160400
 # houseID=ffe9e14822570206cce1fc7259adda71
 # houseID=0004dd3cb11e50530676f77b55262d38
-houseID=ffce180f296526fc7488864978f3019a
+# houseID=ffce180f296526fc7488864978f3019a
+houseID=fe3649f602f371d76660b5cb7219c3d0
 
 ocMapCellSide=0.1 # in m, must be small enough 
 roomMessMean=40 # in num objs per 100m^2
 roomMessSD=10
 frameStep=20 # for poses, Frame period = frameStep * 0.1s
 
-echo 'houseID: '$houseID | tee makedataset_logfile.log
+echo 'houseID: '$houseID | tee $houseID_logfile.log
 
 # Generate .obj and .mtl files from .json
 cd /homes/el216/Workspace/DataSceneNet/Layouts/suncg/house/$houseID
@@ -23,51 +24,54 @@ cd /homes/el216/Workspace/DataSceneNet/Layouts/suncg/house/$houseID
 cd /homes/el216/Workspace/ScriptsSceneNet
 # Convert .obj file to only one floor
 python -u convertToOneFloorObj.py \
-	/homes/el216/Workspace/DataSceneNet/Layouts/suncg/house/$houseID | tee -a makedataset_logfile.log
+	/homes/el216/Workspace/DataSceneNet/Layouts/suncg/house/$houseID | tee -a $houseID_logfile.log
 # Create occupancy map from house obj
 # Outputs: fromOcMap.pckl, roomsLayout.png
 python -u occupancyMap.py \
 	/homes/el216/Workspace/DataSceneNet/Layouts/suncg/house/$houseID \
-	$ocMapCellSide | tee -a makedataset_logfile.log
+	$ocMapCellSide | tee -a $houseID_logfile.log
 # Generate random objects for house
 # Arguments: Room Messiness Mean, SD in num objs per 100m^2
 # Outputs: fromRandomObjects.pckl, roomsLayout+Objects.png, randomObjectsLocations.txt
-python -u randomObjects.py $roomMessMean $roomMessSD $houseID | tee -a makedataset_logfile.log
+python -u randomObjects.py $roomMessMean $roomMessSD $houseID | tee -a $houseID_logfile.log
 # Generate SceneDescription txt from random objects
 # Outputs: scene_description.txt
-python -u generateSceneDesc.py $houseID | tee -a makedataset_logfile.log
+python -u generateSceneDesc.py $houseID | tee -a $houseID_logfile.log
 # Generate Poses.txt from room info from occupancymap.py
 # Outputs: poses.txt
-python -u generatePoses.py $frameStep $houseID | tee -a makedataset_logfile.log
+python -u generatePoses.py $frameStep $houseID | tee -a $houseID_logfile.log
 
 
 # Copy generated files to respective directories
-cp poses.txt /homes/el216/Workspace/DataSceneNet
-cp scene_description.txt /homes/el216/Workspace/DataSceneNet
-find /homes/el216/Workspace/OutputSceneNet -type f -delete
-cp randomObjectsLocations.txt /homes/el216/Workspace/OutputSceneNet
-cp roomsLayout+Objects.png /homes/el216/Workspace/OutputSceneNet
-
+cp $houseID_poses.txt /homes/el216/Workspace/DataSceneNet
+cp $houseID_scene_description.txt /homes/el216/Workspace/DataSceneNet
+#find /homes/el216/Workspace/OutputSceneNet -type f -delete
+output_temp_dir=/homes/el216/Workspace/OutputSceneNet/$houseID
+mkdir $output_temp_dir
+cp $houseID_randomObjectsLocations.txt $output_temp_dir
+cp $houseID_LayoutAndObjects.png $output_temp_dir
 
 # Run renderer
 cd /homes/el216/Workspace/roboteye/build
-./DynamicPose_SceneNet /homes/el216/Workspace/OutputSceneNet \
-/homes/el216/Workspace/DataSceneNet/scene_description.txt | tee -a /homes/el216/Workspace/ScriptsSceneNet/makedataset_logfile.log
+./DynamicPose_SceneNet $output_temp_dir \
+    /homes/el216/Workspace/DataSceneNet/$houseID_scene_description.txt \ 
+    /homes/el216/Workspace/DataSceneNet/$houseID_poses.txt \
+    | tee -a /homes/el216/Workspace/ScriptsSceneNet/$houseID_logfile.log
 
 cd /homes/el216/Workspace/ScriptsSceneNet
-echo 'Frames rendering completed.' | tee -a makedataset_logfile.log
+echo 'Frames rendering completed.' | tee -a $houseID_logfile.log
 
 # Generate new Log file
-python -u processInfoLogForSUNCG.py $houseID | tee -a makedataset_logfile.log
+python -u processInfoLogForSUNCG_temp.py $houseID | tee -a $houseID_logfile.log
 # Generate Label pngs from Instance pngs
-python -u instance2classFromInfoLog.py | tee -a makedataset_logfile.log
+python -u instance2classFromInfoLog_temp.py $houseID | tee -a $houseID_logfile.log
 
 # Copy output to folder named after houseID saved in /scratch drive's output directory
 if [ ! -e /scratch/el216/output_scenenet/$houseID/ ];
 then
     mkdir /scratch/el216/output_scenenet/$houseID
-    cp -r /homes/el216/Workspace/OutputSceneNet/* /scratch/el216/output_scenenet/$houseID
-    echo 'Output files of house '$houseID' copied to /scratch/el216/output_scenenet/'$houseID | tee -a makedataset_logfile.log
+    cp -r /homes/el216/Workspace/OutputSceneNet/$houseID/* /scratch/el216/output_scenenet/$houseID
+    echo 'Output files of house '$houseID' copied to /scratch/el216/output_scenenet/'$houseID | tee -a $houseID_logfile.log
         
 else
     read -p "House "$houseID" output folder exists already. Overwrite contents?" -n 1 -r
@@ -75,12 +79,12 @@ else
     if [[ $REPLY =~ ^[Yy]$ ]]
     then
         rm -r /scratch/el216/output_scenenet/$houseID/* 
-        cp -r /homes/el216/Workspace/OutputSceneNet/* /scratch/el216/output_scenenet/$houseID
-        echo 'Output files of house '$houseID' overwritten to /scratch/el216/output_scenenet/'$houseID | tee -a makedataset_logfile.log
+        cp -r /homes/el216/Workspace/OutputSceneNet/$houseID/* /scratch/el216/output_scenenet/$houseID
+        echo 'Output files of house '$houseID' overwritten to /scratch/el216/output_scenenet/'$houseID | tee -a $houseID_logfile.log
            
     else
         echo 'Output files not moved to /scratch drive' \
-            | tee -a makedataset_logfile.log
+            | tee -a $houseID_logfile.log
     fi
 fi
 
