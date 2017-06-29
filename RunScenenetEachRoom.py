e59808b4ab5c3c1ab8168bb2625aced2398f6d44
Evan Ling <el216@point16.doc.ic.ac.uk>
2017-06-29 19:03:56 +0100

    py script to run scenenet for each room in given house

---
+++ RunScenenetEachRoom.py
@@ -0,0 +1,69 @@
+import numpy as np
+import sys
+import shutil
+import os
+import subprocess
+
+houseID = sys.argv[1]
+
+DynamicPoseSceneNet_path = "/homes/el216/Workspace/scenenet/build/DynamicPose_SceneNet"
+house_temp_dir = '/homes/el216/Workspace/ScriptsSceneNet/' + houseID + '/'
+house_output_temp_dir = "/homes/el216/Workspace/OutputSceneNet/" + houseID + '/'
+
+def mkdir_ifabsent(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+        return True
+    else:
+        return False
+
+f = open(house_temp_dir + houseID + '_lighting.pckl','rb')
+[_, rooms_with_light, _] = pickle.load(f)
+f.close()
+
+f2 = open(house_temp_dir + houseID + '_fromRandomObjects.pckl','rb')
+[_, _, _, _, _, _, nullRooms] = pickle.load(f2)
+f2.close()
+
+for room in rooms_with_light:
+    if room not in nullRooms:
+
+        prefix = houseID + "_" + str(room)
+        scene_desc_filepath = house_temp_dir + prefix + "_scene_description.txt"
+        poses_filepath = house_temp_dir + prefix + "_poses.txt"
+        layout_filepath = house_temp_dir + prefix + "_LayoutAndObjects.png"
+        randObjsTxt_filepath = house_temp_dir + houseID + "_randomObjectsLocations.txt"
+        lightingTxt_filepath = house_temp_dir + houseID + "_lighting.txt"
+
+        mkdir_ifabsent(house_output_temp_dir)
+        room_output_dir = house_output_temp_dir + prefix + "/"
+        mkdir_ifabsent(room_output_dir)
+        mkdir_ifabsent(room_output_dir + "depth/")
+        mkdir_ifabsent(room_output_dir + "photo/")
+        mkdir_ifabsent(room_output_dir + "instance/")
+        shutil.copy2(layout_filepath,room_output_dir)
+        shutil.copy2(randObjsTxt_filepath,room_output_dir)
+        shutil.copy2(lightingTxt_filepath,room_output_dir)
+
+        print "Starting scenenet render of Room",room,"of House",houseID,"...\n"
+
+        print subprocess.check_output([DynamicPoseSceneNet_path, room_output_dir, 
+                                       scene_desc_filepath, poses_filepath])
+
+        print "Completed scenenet render of Room",room,"of House",houseID,"...\n"
+
+
+# #find /homes/el216/Workspace/OutputSceneNet -type f -delete
+# output_temp_dir=/homes/el216/Workspace/OutputSceneNet/${houseID}
+# mkdir -p ${output_temp_dir}
+# mkdir -p ${output_temp_dir}/{depth,photo,instance}
+# cp ${houseID}_randomObjectsLocations.txt $output_temp_dir
+# cp ${houseID}_LayoutAndObjects.png $output_temp_dir
+# cp ${houseID}_lighting.txt $output_temp_dir
+
+# # Run renderer
+# cd /homes/el216/Workspace/scenenet/build
+# ./DynamicPose_SceneNet $output_temp_dir \
+#     /homes/el216/Workspace/DataSceneNet/${houseID}_scene_description.txt \
+#     /homes/el216/Workspace/DataSceneNet/${houseID}_poses.txt \
+#     | tee -a /homes/el216/Workspace/ScriptsSceneNet/logs/${houseID}_run.log
