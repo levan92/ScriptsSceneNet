d16b53ebb1a633787163511b166292deac6b303e
Evan Ling <el216@point03.doc.ic.ac.uk>
2017-07-04 16:26:14 +0100

    fix

--- convert_to_dataset.py
+++ convert_to_dataset.py
@@ -3,6 +3,11 @@ import sys
 import os
 import shutil
 
+def copy_overwrite_tree(src, dst):
+    if os.path.exists(dst):
+        shutil.rmtree(dst)
+    shutil.copytree(src,dst)
+
 def getParentName(path):
     return os.path.basename(os.path.abspath(os.path.join(path, os.pardir)))
 
@@ -13,8 +18,7 @@ house_output_temp_dir = "/homes/el216/Workspace/OutputSceneNet/" + houseID + '/'
 output_dir = "/scratch/el216/output_scenenet/"
 dataset_dir = "/scratch/el216/scenenet_dataset/" + SET + '/' 
 
-if not os.path.exists(output_dir + houseID):
-    shutil.copytree(house_output_temp_dir,output_dir)
+copy_overwrite_tree(house_output_temp_dir, output_dir + houseID)
 
 for root, dirs, files in os.walk(house_output_temp_dir):
     if os.path.basename(root) == "photo":
@@ -53,27 +57,38 @@ for root, dirs, files in os.walk(house_output_temp_dir):
                     # print 'to dst:', dataset_dir + parent + "_" + basename + "_label.png"
             print "Label pngs of",parent,'has been copied to',SET,'datset'
 
-if os.path.exists('dataset_sizes.txt'):
-    with open('dataset_sizes.txt','r') as file:
+if os.path.exists('dataset_overview.txt'):
+    with open('dataset_overview.txt','r') as file:
         data = file.readlines()
 else:
     data = []
 
-if len(data) < 3:
+if len(data) < 5:
     data = []
-    data.append("CNN Dataset Size Overview\n")
+    data.append("CNN Dataset Overview\n")
     data.append("Train Set:\n")
+    data.append("\n")
     data.append("Test Set:\n")
+    data.append("\n")
 
 size = int(len([f for f in os.listdir(dataset_dir)]) / 3.0)
 if SET == "train":
-    data[1] = "Train Set: "+str(size)+"\n"
+    data[1] = "Train Set: size "+str(size)+"\n"
     print 'Train set current size:',str(size)
-elif SET == "test":#
-    data[2] = "Test Set: "+str(size)+"\n"
+    set_houses = data[2].split()
+    if houseID not in set_houses:
+        set_houses.append(houseID)
+    data[2] = ' '.join(set_houses) + "\n"
+   
+elif SET == "test":
+    data[3] = "Test Set: size "+str(size)+"\n"
     print 'Test set current size:',str(size)
+    set_houses = data[4].split()
+    if houseID not in set_houses:
+        set_houses.append(houseID)
+    data[4] = ' '.join(set_houses) + "\n"
 
-with open('dataset_sizes.txt','w') as file:
+with open('dataset_overview.txt','w') as file:
     file.writelines(data)
 
 
