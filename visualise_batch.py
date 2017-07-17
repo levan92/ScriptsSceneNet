8d9ac6c4e96b0a63ef03b1c3c2e2824549502130
Evan Ling <el216@point18.doc.ic.ac.uk>
2017-07-17 18:38:56 +0100

    improvement to usability

--- visualise_batch.py
+++ visualise_batch.py
@@ -3,24 +3,27 @@ import matplotlib.pyplot as plt
 import sys
 import os
 
-root_folder = sys.argv[1]
+def main(root_folder, suffix):
+    vis_folder = os.path.join(root_folder,"Visualisations")
 
-vis_folder = os.path.join(root_folder,"Visualisations")
+    if not os.path.exists(vis_folder):
+        os.mkdir(vis_folder)
 
-if not os.path.exists(vis_folder):
-    os.mkdir(vis_folder)
+    files = next(os.walk(root_folder))[2]
 
-files = next(os.walk(root_folder))[2]
+    for file in files: 
+        if suffix in file:
+            # [basename,ext] = os.path.splitext(file)
+            # if ext == ".png":
+            print "Generating visualisation for",file
+            im_path = os.path.join(root_folder, file)
+            new_name = file.replace(suffix, suffix+"_vis") 
+            new_path = os.path.join(vis_folder, new_name)
+            image = misc.imread(im_path)
+            plt.imshow(image,interpolation='nearest')
+            plt.colorbar()
+            plt.savefig(new_path)
+            plt.clf()
 
-for file in files: 
-    [basename,ext] = os.path.splitext(file)
-    if ext == ".png":
-        print "Generating visualisation for",file
-        im_path = os.path.join(root_folder, file)
-        new_path = os.path.join(vis_folder, basename + "_vis" + ext)
-        image = misc.imread(im_path)
-        plt.imshow(image,interpolation='nearest')
-        plt.colorbar()
-        plt.savefig(new_path)
-
-        plt.clf()
+if __name__ == '__main__':
+    main(sys.argv[1], sys.argv[2])
