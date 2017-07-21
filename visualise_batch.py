70ce765f666fc55d617497e30776faf4487a771e
Evan Ling <el216@point12.doc.ic.ac.uk>
2017-07-21 19:32:46 +0100

    more robust to images with no type labels

--- visualise_batch.py
+++ visualise_batch.py
@@ -13,11 +13,11 @@ def main(root_folder, suffix):
 
     for file in files: 
         if suffix in file:
-            # [basename,ext] = os.path.splitext(file)
+            [basename,ext] = os.path.splitext(file)
             # if ext == ".png":
             print "Generating visualisation for",file
             im_path = os.path.join(root_folder, file)
-            new_name = file.replace(suffix, suffix+"_vis") 
+            new_name = file.replace(ext, "_vis" + ext) 
             new_path = os.path.join(vis_folder, new_name)
             image = misc.imread(im_path)
             plt.imshow(image,interpolation='nearest')
